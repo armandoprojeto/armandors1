624b3e9fff1e9e73fc24a79eaf2e77206b102d80
armando <armandoprojeto123@gmail.com>
2025-07-19 15:37:22 -0300

    adsddgfdgf

--- src/pages/Solicitacoes.jsx
+++ src/pages/Solicitacoes.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 async function enviarMensagemWhatsApp(numero, mensagem) {
     try {
-        const response = await fetch("https://6f308d48f526.ngrok-free.app/enviar-mensagem", {
+        const response = await fetch("https://39a155634620.ngrok-free.app/enviar-mensagem", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ numero, mensagem }),
